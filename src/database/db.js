94a448bf103d7132f32b3c9cd1d1b130a5208e36
Nicolas Mendes <101716911+oNicorasu@users.noreply.github.com>
2023-03-17 22:52:47 -0300

    :sparkles: feat: Variável de Ambiente, .gitignore

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ecommerce', 'root', '', {
+const sequelize = new Sequelize( process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: 'localhost'
 });
